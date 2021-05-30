ea69d0885fc1e7511d7a532fa860d015a55a7a7e
Lars Döpper <lars.doepper@gmail.com>
2021-05-30 09:33:46 +0200

    added dataclasses for attacker, target and weapon

---
+++ unit_classes.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class attacker :
+    ws: int
+    ppm : float
+    hit_mod : int = 0
+    wound_mod : int = 0
+    unit_type : str = 'infantry'
+
+@dataclass
+class target :
+    toughness: int
+    armor: int
+    hp: int
+    unit_type = 'infantry'
+    hit_mod : int = 0
+    wound_mod : int = 0
+
+@dataclass
+class weapon :
+    strength: int
+    ap : int
+    dmg : float
+    dmg_type : str = 'flat' #possibilities are flat, random or mixed, mixed has to be used in combination with dmg_mod
+    dmg_mod : int = 0
